0ba7229794ef906ef1dcfbae1e9d52529cac85ee
pianoricky@gmail.com <pianoricky@gmail.com>
2016-04-06 19:51:13 -0500

    Thread ya corre

--- src/quimica/Quimica.java
+++ src/quimica/Quimica.java
@@ -7,21 +7,53 @@
 package quimica;
 
 import javax.swing.JFrame;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Maribel
  */
-public class Quimica extends JFrame {
+public class Quimica extends JFrame implements Runnable {
     
-    private final static int WINDOW_WIDTH = 1000;
-    private final static int WINDOW_HEIGHT = 600;
+    public final static int WINDOW_WIDTH = 1000;
+    public final static int WINDOW_HEIGHT = 600;
+    
+    private int score;
+    private int lives;
+    
+    private PersonajePrincipal personaje;
+    private ArrayList<Respuesta> respuestas;
     
     public Quimica() {
+        initUI();
+        
+        score = 0;
+        lives = 3;
+        
+        Thread thread = new Thread(this);
+        thread.start();
+    }
+    
+    public void initUI() {
         setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         setTitle("Quimica");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+        setResizable(true);
+    }
+    
+    public void run() {
+        while (true) {
+            System.out.println("Hello World");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Quimica.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
     }
     
     /**
